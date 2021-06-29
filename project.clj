915b181209e9f55ad53e3212d6d26c2f97076a11
jem <michael.jerger@meissa-gmbh.de>
2021-06-29 17:58:32 +0200

    Version 1.12.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/mastodon-bot "1.12.1-SNAPSHOT"
+(defproject dda/mastodon-bot "1.12.1"
   :description "Bot to publish twitter, tumblr or rss posts to an mastodon account."
   :url "https://github.com/yogthos/mastodon-bot"
   :author "Dmitri Sotnikov"
